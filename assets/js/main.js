e62b26948fa37e1bf1ab238ce568bab3df2252d2
azm1med <batuongweb@gmail.com>
2022-06-23 10:09:00 +0700

    offshore-page--new

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,28 @@ const mavMb = () => {
     }
 }
 mavMb();
+
+function odcTab(e) {
+    const odcTabId = e.id;
+    const odcTabGet = document.getElementById(odcTabId);
+    const odcSplit = odcTabId.split('-');
+    const odcPannel = odcSplit[0] + '-' + odcSplit[1] + '-pannel-' + odcSplit[2];
+    const odcPannelGet = document.getElementById(odcPannel);
+    
+    if(!odcTabGet.classList.contains('active')) {
+        odcReset();
+        odcTabGet.classList.add('active');
+        odcPannelGet.classList.add('active'); 
+    } 
+}
+
+function odcReset() {
+    const odcCNav = document.getElementsByClassName('odc_team-nav-item');
+    const odcCPannel = document.getElementsByClassName('odc_team-pannel-item');
+    
+    for (var i = 0; i < odcCNav.length; i++) {
+        odcCNav[i].classList.remove('active');
+        odcCPannel[i].classList.remove('active');
+    }
+
+}
